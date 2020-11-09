3c6f184e39e39853e3319c796888a3af1e68831e
alperensert <alperenssrt@gmail.com>
2020-11-09 13:55:11 +0300

    added FunCaptchaTask & Proxyless for usage

--- python_capmonster/__init__.py
+++ python_capmonster/__init__.py
@@ -2,4 +2,6 @@ from .exceptions import CapmonsterException
 from .ImageToTextTask import ImageToTextTask
 from .NoCaptchaTaskProxyless import NoCaptchaTaskProxyless
 from .RecaptchaV3TaskProxyless import RecaptchaV3TaskProxyless
+from .FunCaptchaTask import FunCaptchaTask
+from .FunCaptchaTaskProxyless import FunCaptchaTaskProxyless
 from .NoCaptchaTask import NoCaptchaTask
