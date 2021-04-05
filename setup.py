e249e6076f55c916aa54bbc2106f15c4444e663c
alperensert <alperenssrt@gmail.com>
2021-04-05 23:39:46 +0300

    updated 1.3.1

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
 from distutils.core import setup
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
 
 setup(
     name="capmonster_python",
@@ -6,6 +10,8 @@ setup(
     packages=["capmonster_python"],
     url="https://github.com/alperensert/capmonster_python",
     download_url="https://github.com/alperensert/capmonster_python/archive/v1.3.0.tar.gz",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     license="MIT",
     author="Alperen Sert",
     author_email="alperenssrt@gmail.com",
