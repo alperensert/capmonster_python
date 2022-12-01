d63375587b5f233b7709ebe09a5729480ec960cb
Alperen Sert <63921520+alperensert@users.noreply.github.com>
2022-12-01 03:50:58 +0300

    added docs via gatsby deploy

---
+++ docs/gatsby-config.js
@@ -0,0 +1,53 @@
+module.exports = {
+  siteMetadata: {
+    siteTitle: `capmonster-python docs`,
+    defaultTitle: `capmonster-python Docs`,
+    siteTitleShort: `capmonster-python Docs`,
+    siteDescription: `capmonster-python documentation for capmonster.cloud`,
+    siteUrl: `https://docs.alperenn.com`,
+    siteAuthor: `@alperensert`,
+    siteImage: `/banner.png`,
+    siteLanguage: `en`,
+    themeColor: `#8257E6`,
+    basePath: `/`,
+  },
+  plugins: [
+    {
+      resolve: `@rocketseat/gatsby-theme-docs`,
+      options: {
+        configPath: `src/config`,
+        docsPath: `src/docs`,
+        yamlFilesPath: `src/yamlFiles`,
+        repositoryUrl: `https://github.com/jpedroschmitz/rocketdocs`,
+        baseDir: `examples/gatsby-theme-docs`,
+        gatsbyRemarkPlugins: [],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Rocket Docs`,
+        short_name: `Rocket Docs`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        display: `standalone`,
+        icon: `static/favicon.png`,
+      },
+    },
+    `gatsby-plugin-sitemap`,
+    // {
+    //   resolve: `gatsby-plugin-google-analytics`,
+    //   options: {
+    //     trackingId: `YOUR_ANALYTICS_ID`,
+    //   },
+    // },
+    `gatsby-plugin-remove-trailing-slashes`,
+    {
+      resolve: `gatsby-plugin-canonical-urls`,
+      options: {
+        siteUrl: `https://capmonster-python.alperenn.com`,
+      },
+    },
+    `gatsby-plugin-offline`,
+  ],
+};
